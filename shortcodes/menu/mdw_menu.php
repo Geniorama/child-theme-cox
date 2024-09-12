8966b11b0a5a716aac5f1ad441b246cfd0cfae21
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-11 23:46:34 -0500

    feat(menu): Agrega el shortcode para el menu tipo hamburguesa

---
+++ shortcodes/menu/mdw_menu.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Custom menu
+ */
+function mostrar_menu_main_menu()
+{
+  wp_enqueue_style('mdw-menu-style', get_stylesheet_directory_uri() . '/shortcodes/menu/mdw_menu.css', array(), '1.0');
+  wp_enqueue_script('mdw-menu-script', get_stylesheet_directory_uri() . '/shortcodes/menu/mdw_menu.js', array('jquery'), null, true);
+
+  // Obtiene el menú con el nombre 'main menu'
+  $menu_name = 'main menu';
+  $menu = wp_get_nav_menu_object($menu_name);
+
+  // Si el menú existe, genera la lista
+  if ($menu) {
+    $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+    // Organiza los elementos en una estructura jerárquica
+    $menu_list = build_menu_hierarchy($menu_items);
+
+    // Genera el HTML del menú
+    $logoURL = "/wp-content/uploads/2024/09/66Recurso-3.svg";
+    $htmlmenu = generate_menu_html($menu_list);
+    $html = '';
+    $html .= "
+      <div id='mdw__megamenu' class='mdw__megamenu'>
+        <div class='mdw__megamenu_header'>
+          <img src='$logoURL' alt='Logo' width='100' height='80' class='mdw__megamenu_logo'>
+        </div>
+        $htmlmenu
+      </div>
+    ";
+    return $html;
+  }
+}
+add_shortcode('mostrar_menu', 'mostrar_menu_main_menu');
+
+// Función recursiva para generar el HTML del menú
+function generate_menu_html($items, $parent_id = 0)
+{
+  $output = '<ul class="mdw__menu_level">';
+  $iconHaveChildren = "/wp-content/uploads/2024/09/a-bRecurso-3.svg";
+
+  foreach ($items as $item) {
+    // Genera el elemento de menú
+    $output .= '<li class="mdw__menu_item"><a href="' . esc_url($item->url) . '">' . esc_html($item->title) . '</a>';
+
+    // Si el elemento tiene hijos, llama recursivamente
+    if (!empty($item->children)) {
+      $output .= generate_menu_html($item->children);
+      $output .= "<img src='$iconHaveChildren' alt='Logo' width='14' height='14' class='mdw__megamenu_logo'>";
+    }
+
+    $output .= '</li>';
+  }
+
+  $output .= '</ul>';
+  return $output;
+}
+
+// Función para construir la jerarquía de menús
+function build_menu_hierarchy($menu_items, $parent_id = 0)
+{
+  $hierarchy = [];
+
+  foreach ($menu_items as $item) {
+    if ($item->menu_item_parent == $parent_id) {
+      $children = build_menu_hierarchy($menu_items, $item->ID); // Encuentra hijos
+      if ($children) {
+        $item->children = $children; // Asigna hijos al item
+      }
+      $hierarchy[] = $item;
+    }
+  }
+
+  return $hierarchy;
+}
