a6c873b657648d1b107bc64c1564aa7aef657da0
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-16 00:46:46 -0500

    feat(menu): Actualiza animación, para que cada nivel del menú salga de izquierda a derecha y no de arriba a abajo

--- shortcodes/menu/mdw_menu.js
+++ shortcodes/menu/mdw_menu.js
@@ -1,30 +1,43 @@
 jQuery(document).ready(function($) {
-  $(document).on('elementor/popup/show', function() {
-    // Oculta todos los submenús inicialmente
-    $('#mdw__megamenu li ul').hide(); // Selecciona todos los <ul> que son hijos de <li>
-
-    // Al hacer hover sobre un elemento de primer nivel
-    $('#mdw__megamenu ul > li').hover(
-        function() {
-            // Muestra el submenú de primer nivel (hijos inmediatos)
-            $(this).children('ul').stop(true, true).slideDown(300);
-        },
-        function() {
-            // Oculta el submenú de primer nivel cuando se quita el hover
-            $(this).children('ul').stop(true, true).slideUp(300);
-        }
-    );
-
-    // Al hacer hover sobre un submenú de segundo nivel
-    $('#mdw__megamenu li ul li').hover(
-        function() {
-            // Muestra el submenú de segundo nivel (hijos inmediatos)
-            $(this).children('ul').stop(true, true).slideDown(300);
-        },
-        function() {
-            // Oculta el submenú de segundo nivel cuando se quita el hover
-            $(this).children('ul').stop(true, true).slideUp(300);
-        }
-    );
+    $(document).on('elementor/popup/show', function() {
+      // Oculta todos los submenús inicialmente
+      $('#mdw__megamenu li ul').css('width', '0');
+  
+      // Al hacer hover sobre un elemento de primer nivel
+      $('#mdw__megamenu ul > li').hover(
+          function() {
+              // Muestra el submenú de primer nivel (hijos inmediatos)
+              $(this).children('ul').stop(true, true).css({
+                'width': '28vw',
+                'left': '28vw',
+              }); // Ajusta el ancho según sea necesario
+          },
+          function() {
+              // Oculta el submenú de primer nivel cuando se quita el hover
+              $(this).children('ul').stop(true, true).css({
+                'width': '0',
+                'left': '0',
+              });
+          }
+      );
+  
+      // Al hacer hover sobre un submenú de segundo nivel
+      $('#mdw__megamenu li ul li').hover(
+          function() {
+              // Muestra el submenú de segundo nivel (hijos inmediatos)
+              $(this).children('ul').stop(true, true).css({
+                'width': '28vw',
+                'left': '28vw',
+              }); // Ajusta el ancho según sea necesario
+          },
+          function() {
+              // Oculta el submenú de segundo nivel cuando se quita el hover
+              $(this).children('ul').stop(true, true).css({
+                'width': '0',
+                'left': '-28vw',
+              });
+          }
+      );
+    });
   });
-});
+  
